8f590fde2030b30130aad5f2ee8f80445b856445
gielwijgergangs@users.noreply.github.com <giel@cyclesoftware.nl>
2026-02-23 11:47:55 +0100

    repo

---
+++ src/Handler.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+
+namespace Manticoresearch\Buddy\Plugin\Template;
+
+use Manticoresearch\Buddy\Core\Plugin\BaseHandler;
+use Manticoresearch\Buddy\Core\Task\Task;
+use Manticoresearch\Buddy\Core\Task\Column;
+use Manticoresearch\Buddy\Core\Task\TaskResult;
+use RuntimeException;
+use parallel\Runtime;
+
+final class Handler extends BaseHandler
+{
+	/**
+	 * Initialize the executor
+	 *
+	 * @param Payload $payload
+	 * @return void
+	 */
+	public function __construct(public Payload $payload)
+	{
+	}
+
+	/**
+	 * Process the request
+	 *
+	 * @return Task
+	 * @throws RuntimeException
+	 */
+	public function run(Runtime $runtime): Task
+	{
+		$taskFn = static function (): TaskResult {
+			return TaskResult::withRow(
+				[
+					'index' => 'rt_type_foo',
+					'is_running' => 0,
+					'result' => $this->payload->path,
+				]
+			)->column(
+				'index', Column::String,
+				'is_running', Column::Long,
+				'result', Column::String,
+			);
+		};
+
+		return Task::createInRuntime(
+			$runtime, $taskFn, []
+		)->run();
+	}
+
+	/**
+	 * @return array<string>
+	 */
+	public function getProps(): array
+	{
+		return [];
+	}
+}
