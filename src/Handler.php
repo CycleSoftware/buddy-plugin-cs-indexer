60a08b7cfa4418f0c4b6ba8e170c09889b865de1
DaanBaars <daan@cyclesoftware.nl>
2026-03-11 15:35:26 +0100

    supress errors and continue

--- src/Handler.php
+++ src/Handler.php
@@ -100,13 +100,14 @@ final class Handler extends BaseHandler
 					$index_name = trim($index_name, ' "');
 					$file = trim($file);
 					[$file] = explode(' ', $file);
-					$contents = file_get_contents($file);
+					$contents = @file_get_contents($file);
 					if (!$contents) {
 						@file_put_contents(
 							sys_get_temp_dir() . "/indexer_buddy_error.log",
 							date('Y-m-d H:i:s') . ' - ' . json_encode([$file, $line]),
 							FILE_APPEND
 						);
+						continue;
 					}
 					$rows[] = [
 						'pid' => $parts[0],
