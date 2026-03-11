469510b5b2ff249d3c60a4e20ba2c148788cc1d4
DaanBaars <daan@cyclesoftware.nl>
2026-03-11 16:31:25 +0100

    debug

--- src/Handler.php
+++ src/Handler.php
@@ -89,14 +89,13 @@ final class Handler extends BaseHandler
 			if ($this->payload->path === 'show indexer status' || $this->payload->path === 'indexer status') {
 				// "show indexer status"
 
-				exec('ps -eo pid,cmd', $output, $result);
+				exec('ps -eo pid,cmd ww | grep indexer', $output, $result);
 
 				@file_put_contents(
 					sys_get_temp_dir() . "/indexer_buddy_error.log",
 					date('Y-m-d H:i:s') . ' - ' . json_encode($output).\PHP_EOL,
 					FILE_APPEND
 				);
-
 
 
 				array_shift($output); // remove header
