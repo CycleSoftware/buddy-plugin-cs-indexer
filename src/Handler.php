996394947c2c616e4161083da86fcf826ae352ca
DaanBaars <daan@cyclesoftware.nl>
2026-03-16 14:44:02 +0100

    wip

--- src/Handler.php
+++ src/Handler.php
@@ -42,115 +42,13 @@ final class Handler extends BaseHandler
 			// "run indexer rotate index_name"
 			// "run indexer rotate"
 			if (str_starts_with($this->payload->path, 'indexer rotate')) {
-				$parts = explode(' ', $this->payload->path);
-				$index_name = $parts[2] ?? null;
-				if (count($parts) > 3) {
-					return TaskResult::withError($this->payload->path . ' is not a valid index command');
-				}
-				if (count($parts) > 2 && $index_name !== null && $index_name !== preg_replace("/[^a-zA-Z0-9" . preg_quote('_-') . "]+/", "", $index_name)) {
-					return TaskResult::withError('"' . $index_name . '" is not a valid index name');
-				}
-				if (isset($index_name) && str_starts_with($index_name, '-')) {
-					// do not all other options to indexer
-					return TaskResult::withError($index_name . ' is not a valid index name');
-				}
-				$index_name = $index_name ?? '--all';
-				$reference = uniqid();
-				$file = tempnam(sys_get_temp_dir(), "indexer_" . $reference . '_');
-				exec('bash -c "indexer --rotate "' . $index_name . '" > ' . $file . ' 2>&1" > /dev/null 2>&1 & echo $!', $output, $return);
-				if ((int)$return !== 0) {
-					return TaskResult::withError('error starting the indexer: ' . $return);
-				}
-				$row = [
-					'pid' => trim($output[0]),
-					'reference' => $reference,
-					'payload' => $this->payload->path,
-				];
-				if($index_name !== '--all') {
-					$row['index'] = $index_name;
-				}
-				$task_result = TaskResult::withRow(
-					$row
-				)->column(
-					'pid',
-					Column::String,
-				)->column(
-					'reference',
-					Column::String,
-				)->column(
-					'payload',
-					Column::String,
-				);
-				if($index_name !== '--all') {
-					$task_result->column(
-						'index',
-						Column::String,
-					);
-				}
-				return $task_result;
+				return $this->indexer_rotate();
 			}
-
 			if ($this->payload->path === 'show indexer status' || $this->payload->path === 'indexer status') {
-				// "show indexer status"
-				exec('ps -eo pid,cmd ww | grep -e \'indexer --rotate\'', $output, $result);
-				//array_shift($output); // remove header
-				$rows = [];
-
-				foreach ($output as $line) {
-					if (!str_contains($line, ' > ')) {
-						continue;
-					}
-					$parts = preg_split('/\s+/', trim($line), 2);
-					if ($parts === false) {
-						continue;
-					}
-					[$before_output_file, $file] = explode(' > ', $parts[1]);
-					[, $index_name] = explode('--rotate', $before_output_file);
-					$index_name = trim($index_name, ' "');
-					$file = trim($file);
-					[$file] = explode(' ', $file);
-					if (!is_file($file)) {
-						@file_put_contents(
-							sys_get_temp_dir() . "/indexer_buddy_error.log",
-							date('Y-m-d H:i:s') . ' - is_file - ' . json_encode([$file, $line]) . \PHP_EOL,
-							FILE_APPEND
-						);
-					}
-					$contents = @file_get_contents($file);
-					if (!$contents) {
-						@file_put_contents(
-							sys_get_temp_dir() . "/indexer_buddy_error.log",
-							date('Y-m-d H:i:s') . ' - false content - ' . json_encode([$file, $line]) . \PHP_EOL,
-							FILE_APPEND
-						);
-						$contents = 'can\'t read indexer file';
-					}
-					$rows[] = [
-						'pid' => $parts[0],
-						'index' => $index_name === '--all' ? null : $index_name,
-						'command' => implode(' ', array_slice($parts, 10)),
-						'output' => !$contents ? null : $contents,
-						'payload' => $this->payload->path,
-					];
-				}
-				return TaskResult::withData(
-					$rows
-				)->column(
-					'pid',
-					Column::String,
-				)->column(
-					'index',
-					Column::String,
-				)->column(
-					'command',
-					Column::String,
-				)->column(
-					'output',
-					Column::String,
-				)->column(
-					'payload',
-					Column::String,
-				);
+				return $this->indexer_show_status();
+			}
+			if($this->$this->payload->path === '@@node-id') {
+					return $this->get_node_id();
 			}
 			return TaskResult::withError('unknown request: ' . $this->payload->path);
 		};
@@ -165,5 +63,143 @@ final class Handler extends BaseHandler
 	public function getProps(): array
 	{
 		return [];
+	}
+
+	/**
+	 * @return TaskResult|null
+	 */
+	protected function get_node_id(): ?TaskResult
+	{
+		if(!is_file('/etc/manticore/node-id')) {
+			throw new RuntimeException('no node id file');
+		}
+		return TaskResult::withRow(
+			[
+				'node-id' => (int)file_get_contents('/etc/manticore/node-id'),
+			]
+		)->column(
+			'node-id',
+			Column::Long,
+		);
+	}
+
+	/**
+	 * @return TaskResult
+	 */
+	protected function indexer_rotate(): TaskResult
+	{
+		$parts = explode(' ', $this->payload->path);
+		$index_name = $parts[2] ?? null;
+		if (count($parts) > 3) {
+			return TaskResult::withError($this->payload->path . ' is not a valid index command');
+		}
+		if (count($parts) > 2 && $index_name !== null && $index_name !== preg_replace("/[^a-zA-Z0-9" . preg_quote('_-') . "]+/", "", $index_name)) {
+			return TaskResult::withError('"' . $index_name . '" is not a valid index name');
+		}
+		if (isset($index_name) && str_starts_with($index_name, '-')) {
+			// do not all other options to indexer
+			return TaskResult::withError($index_name . ' is not a valid index name');
+		}
+		$index_name = $index_name ?? '--all';
+		$reference = uniqid();
+		$file = tempnam(sys_get_temp_dir(), "indexer_" . $reference . '_');
+		exec('bash -c "indexer --rotate "' . $index_name . '" > ' . $file . ' 2>&1" > /dev/null 2>&1 & echo $!', $output, $return);
+		if ((int)$return !== 0) {
+			return TaskResult::withError('error starting the indexer: ' . $return);
+		}
+		$row = [
+			'pid' => trim($output[0]),
+			'reference' => $reference,
+			'payload' => $this->payload->path,
+		];
+		if ($index_name !== '--all') {
+			$row['index'] = $index_name;
+		}
+		$task_result = TaskResult::withRow(
+			$row
+		)->column(
+			'pid',
+			Column::String,
+		)->column(
+			'reference',
+			Column::String,
+		)->column(
+			'payload',
+			Column::String,
+		);
+		if ($index_name !== '--all') {
+			$task_result->column(
+				'index',
+				Column::String,
+			);
+		}
+		return $task_result;
+	}
+
+	/**
+	 * @return TaskResult
+	 */
+	protected function indexer_show_status(): TaskResult
+	{
+		// "show indexer status"
+		exec('ps -eo pid,cmd ww | grep -e \'indexer --rotate\'', $output, $result);
+		//array_shift($output); // remove header
+		$rows = [];
+
+		foreach ($output as $line) {
+			if (!str_contains($line, ' > ')) {
+				continue;
+			}
+			$parts = preg_split('/\s+/', trim($line), 2);
+			if ($parts === false) {
+				continue;
+			}
+			[$before_output_file, $file] = explode(' > ', $parts[1]);
+			[, $index_name] = explode('--rotate', $before_output_file);
+			$index_name = trim($index_name, ' "');
+			$file = trim($file);
+			[$file] = explode(' ', $file);
+			if (!is_file($file)) {
+				@file_put_contents(
+					sys_get_temp_dir() . "/indexer_buddy_error.log",
+					date('Y-m-d H:i:s') . ' - is_file - ' . json_encode([$file, $line]) . \PHP_EOL,
+					FILE_APPEND
+				);
+			}
+			$contents = @file_get_contents($file);
+			if (!$contents) {
+				@file_put_contents(
+					sys_get_temp_dir() . "/indexer_buddy_error.log",
+					date('Y-m-d H:i:s') . ' - false content - ' . json_encode([$file, $line]) . \PHP_EOL,
+					FILE_APPEND
+				);
+				$contents = 'can\'t read indexer file';
+			}
+			$rows[] = [
+				'pid' => $parts[0],
+				'index' => $index_name === '--all' ? null : $index_name,
+				'command' => implode(' ', array_slice($parts, 10)),
+				'output' => !$contents ? null : $contents,
+				'payload' => $this->payload->path,
+			];
+		}
+		return TaskResult::withData(
+			$rows
+		)->column(
+			'pid',
+			Column::String,
+		)->column(
+			'index',
+			Column::String,
+		)->column(
+			'command',
+			Column::String,
+		)->column(
+			'output',
+			Column::String,
+		)->column(
+			'payload',
+			Column::String,
+		);
 	}
 }
