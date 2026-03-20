78cdce9517ed2dacceb671ed2d21e3d06d458fa8
DaanBaars <daan@cyclesoftware.nl>
2026-03-20 11:47:34 +0100

    debug

--- src/Handler.php
+++ src/Handler.php
@@ -153,6 +153,14 @@ final class Handler extends BaseHandler
 				continue;
 			}
 			$parts = preg_split('/\s+/', trim($line), 2);
+
+
+			@file_put_contents(
+				sys_get_temp_dir() . "/indexer_buddy_error.log",
+				date('Y-m-d H:i:s') . ' - false content - ' . json_encode([$line,$parts]) . \PHP_EOL,
+				FILE_APPEND
+			);
+
 			if ($parts === false) {
 				continue;
 			}
